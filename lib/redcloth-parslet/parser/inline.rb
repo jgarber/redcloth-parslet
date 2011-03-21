66baeb6cc1880a57fb1f9b47c1150813455bc4fc
Jason Garber <jg@jasongarber.com>
2011-03-21 11:18:48 -0600

    wip : working but way too many named atoms => too atomic for my tastes

--- lib/redcloth-parslet/parser/inline.rb
+++ lib/redcloth-parslet/parser/inline.rb
@@ -1,34 +1,33 @@
 class RedClothParslet::Parser::Inline < Parslet::Parser
   
-  rule(:inline) do
-    inline_element.repeat(1)
+  def inline(char_exception = nil)
+    (inline_element(char_exception) >> (inline_spaces.as(:leading_space) >> inline_element(char_exception) >> punct?.as(:punct)).repeat)
   end
   
-  # Inline elements are terms (words) divided by spaces. The term's
-  # trailing spaces (if any) are captured by the term.
-  rule(:inline_element) do
+  # Inline elements are terms (words) divided by spaces.
+  def inline_element(char_exception = nil)
     term |
-    plain_phrase
+    word(char_exception)
   end
   
-  def inline_except(char_exception = nil)
-    (term | plain_phrase(char_exception)).repeat(1)
-  end
+  # def inline_except(char_exception = nil)
+  #   (term | plain_phrase(char_exception)).repeat(1)
+  # end
   
   rule(:term) do
     strong.as(:strong)
   end
   
-  rule(:strong) { (str('*') >> inline_except(end_strong).as(:content) >> end_strong) >> inline_sp?.as(:tr_sp) }
+  rule(:strong) { (str('*') >> inline(end_strong).as(:content) >> end_strong) }
   rule(:end_strong) { str('*') >> match("[a-zA-Z0-9]").absent? }
 
-  def plain_phrase(char_exception = nil)
-    (word(char_exception) >> (inline_sp >> term.absent? >> word(char_exception)).repeat >> inline_sp?).as(:s)
-  end
+  # def plain_phrase(char_exception = nil)
+  #   (word(char_exception) >> (inline_sp >> term.absent? >> word(char_exception)).repeat >> inline_sp?).as(:s)
+  # end
   
   def word(char_exception = nil)
     char = char_exception.nil? ? mchar : (char_exception.absent? >> mchar)
-    char.repeat(1)
+    term.absent? >> char.repeat(1).as(:s)
   end
   
   # rule(:plain_phrase) { (word >> (inline_sp >> plain_word).repeat >> inline_sp?).as(:s) }
@@ -38,10 +37,10 @@ class RedClothParslet::Parser::Inline < Parslet::Parser
   #   str("*"))
   # }
   rule(:mchar) { match('\S') }
-  rule(:inline_sp?) { inline_sp.repeat }
-  rule(:inline_sp) { match('[ \t]').repeat(1) }
-  # rule(:punct) { match('[?.!]') }
-  # rule(:punct?) { punct.repeat }
+  # rule(:inline_spaces?) { inline_sp.repeat }
+  rule(:inline_spaces) { match('[ \t]').repeat(1) }
+  rule(:punct) { match('[?.!]') }
+  rule(:punct?) { punct.repeat }
   
   root(:inline)
 end
