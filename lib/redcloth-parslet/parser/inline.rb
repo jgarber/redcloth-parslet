1ad27309d5cdedf8a249b56756eab554d563ee4e
Jason Garber <jg@jasongarber.com>
2011-03-21 12:18:51 -0600

    Successful parsing of strong mixed in with plain phrases

--- lib/redcloth-parslet/parser/inline.rb
+++ lib/redcloth-parslet/parser/inline.rb
@@ -1,29 +1,31 @@
 class RedClothParslet::Parser::Inline < Parslet::Parser
   
+  root(:inline)
   rule(:inline) do
-    inline_element.repeat(1)
+    inline_except(nil)
   end
   
   # Inline elements are terms (words) divided by spaces. The term's
   # trailing spaces (if any) are captured by the term.
-  rule(:inline_element) do
-    term |
-    plain_phrase
-  end
   
   def inline_except(char_exception = nil)
-    (term | plain_phrase(char_exception)).repeat(1)
+    inline_sp.absent? >> (term | plain_phrase(char_exception)).repeat(1)
   end
   
   rule(:term) do
-    strong.as(:strong)
+    strong
   end
   
-  rule(:strong) { (str('*') >> inline_except(end_strong).as(:content) >> end_strong) >> inline_sp?.as(:tr_sp) }
+  rule(:strong) { (str('*').as(:inline) >> inline_except(end_strong).as(:content) >> end_strong) }
   rule(:end_strong) { str('*') >> match("[a-zA-Z0-9]").absent? }
 
   def plain_phrase(char_exception = nil)
-    (word(char_exception) >> (inline_sp >> term.absent? >> word(char_exception)).repeat >> inline_sp?).as(:s)
+    (
+      inline_sp? >> 
+      word(char_exception) >> 
+      (inline_sp >> term.absent? >> word(char_exception)).repeat >> 
+      safe_trailing_space(char_exception).repeat
+    ).as(:s)
   end
   
   def word(char_exception = nil)
@@ -31,17 +33,11 @@ class RedClothParslet::Parser::Inline < Parslet::Parser
     char.repeat(1)
   end
   
-  # rule(:plain_phrase) { (word >> (inline_sp >> plain_word).repeat >> inline_sp?).as(:s) }
-  # rule(:plain_word) { strong.absent? >> word }
-  # rule(:word) {
-  #   ((end_strong.absent? >> mchar).repeat(1) |
-  #   str("*"))
-  # }
+  def safe_trailing_space(char_exception = nil)
+    char_exception.nil? ? match('[ \t]') : (match('[ \t]') >> char_exception.absent?)
+  end
+
   rule(:mchar) { match('\S') }
   rule(:inline_sp?) { inline_sp.repeat }
   rule(:inline_sp) { match('[ \t]').repeat(1) }
-  # rule(:punct) { match('[?.!]') }
-  # rule(:punct?) { punct.repeat }
-  
-  root(:inline)
 end
--- spec/parser/inline_spec.rb
+++ spec/parser/inline_spec.rb
@@ -3,68 +3,63 @@ describe RedClothParslet::Parser::Inline do
   
   context "plain text" do
     it { should parse("Just plain text.").
-      as({:s => "Just plain text."}) }
+      as([{:s => "Just plain text."}]) }
 
     it { should parse("One sentence. Two.").
-      as({:s => "One sentence. Two."}) }
+      as([{:s => "One sentence. Two."}]) }
   end
   
   describe "#strong" do
     it "should consume a strong word" do
-      parser.strong.should parse('*hey*', {:trace => true})
+      parser.strong.should parse('*hey*')
     end
 
     it "should consume a strong phrase" do
       parser.strong.should parse('*hey now!*')
     end
-    
-    it "should capture trailing space" do
-      parser.strong.should parse('*hey* ').
-        as([{:strong => [{:s => "hey"}]}, {:tr_sp => ' '}])
-    end
-
-    it "should capture trailing space" do
-      parser.strong.should parse('*hey there!* ').
-        as([{:strong => [{:s => "hey "}, {:s => "there!"}]}, {:tr_sp => ' '}])
-    end
   end
   
   context "strong phrase" do
     it { should parse("*strong phrase*").
-      as({:strong => {:s => "strong phrase"}}) }
+      as([{:inline => "*", :content => [:s => "strong phrase"]}]) }
     
     it "should parse a strong word surrounded by plain text" do
       subject.should parse("plain *strong* plain").
       as([{:s => "plain "}, 
-          {:strong => {:s => "strong"}},
+          {:inline => "*", :content => [{:s => "strong"}]},
           {:s => " plain"}])
     end
     
     it "should parse a strong phrase surrounded by plain text" do
       subject.should parse("plain *strong phrase* plain").
       as([{:s => "plain "}, 
-          {:strong => {:s => "strong phrase"}},
+          {:inline => "*", :content => [{:s => "strong phrase"}]},
           {:s => " plain"}])
     end
     
     it "should allow a strong phrase at the end of a sentence before punctuation" do
       subject.should parse("Are you *veg*an*?", {:trace => true}).
-        as([{:s => "Are you "}, {:strong => {:s => "veg*an"}}, {:s => "?"}])
+        as([{:s => "Are you "}, {:inline => "*", :content => [:s => "veg*an"]}, {:s => "?"}])
     end
     
     it "should parse a phrase with standalone asterisks that is not a strong phrase" do
       subject.should parse("yes * we * can").
-        as({:s => "yes * we * can"})
+        as([{:s => "yes * we * can"}])
     end
 
     it "should parse a phrase with asterisky words that is not a strong phrase" do
       subject.should parse("The veg*an options are for veg*ans only.").
-        as({:s => "The veg*an options are for veg*ans only."})
+        as([{:s => "The veg*an options are for veg*ans only."}])
     end
 
     it "should parse a phrase that is not a strong because it has space at the end" do
       subject.should parse("yeah *that's * it!").
-        as({:s => "yeah *that's * it!"})
+        as([{:s => "yeah *that's * it!"}])
+    end
+    
+    it "should parse a phrase that is not a strong because it has space at the beginning" do
+      subject.should parse("oh, * here* it is!").
+        as([{:s => "oh, * here* it is!"}])
     end
   end
   
