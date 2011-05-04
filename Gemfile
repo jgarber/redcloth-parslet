33102939bb628cfc8148d69e93439a579bab79e9
Jason Garber <jg@jasongarber.com>
2011-05-04 10:18:58 -0600

    Use jgarber/parslet@da3ff3c transform_instance_eval branch to eliminate hack needed for accessing @doc in transform rules.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "http://rubygems.org"
 gemspec
 
+gem 'parslet', :git => 'git@github.com:jgarber/parslet.git', :branch => 'transform_instance_eval'
+
 group :development do
   gem 'ruby-debug'
   gem 'autotest-standalone'
--- lib/redcloth-parslet/transform/inline.rb
+++ lib/redcloth-parslet/transform/inline.rb
@@ -1,38 +1,25 @@
 class RedClothParslet::Transform::Inline < Parslet::Transform
-  
-  rule(:s => simple(:s)) do 
-    Nokogiri::XML::Text.new(s, doc)
-  end
-  
-  rule(:inline => simple(:i), :content => sequence(:c), :attributes => subtree(:a)) do
-    name = {'*'=>'strong', '_'=>'em'}[i]
-    Nokogiri::XML::Element.new(name, doc) do |n|
-      a.each do |attrs|
-        attrs.each do |k,v|
-          n[k.to_s] = ((n[k.to_s] || '').split(/\s/) + [v]).join(' ')
-        end
-      end
-      n.add_child(Nokogiri::XML::NodeSet.new(doc, c))
-    end
-  end
+  attr_accessor :doc
 
   def initialize(doc)
     @doc = doc
     super()
   end
-  # Overrides Parslet::Transform#transform_elt to inject doc 
-  # into bindings. Wouldn't have to do this if #call_on_match 
-  # were evaluated in the context of the transform instance.
-  def transform_elt(elt) # :nodoc: 
-    rules.each do |pattern, block|
-      if bindings=pattern.match(elt)
-        bindings[:doc] = @doc
-        # Produces transformed value
-        return pattern.call_on_match(bindings, block)
-      end
-    end
-    
-    # No rule matched - element is not transformed
-    return elt
+
+  rule(:s => simple(:s)) do |match|
+    Nokogiri::XML::Text.new(match[:s], doc)
   end
+  
+  rule(:inline => simple(:i), :content => sequence(:c), :attributes => subtree(:a)) do |match|
+    name = {'*'=>'strong', '_'=>'em'}[match[:i]]
+    Nokogiri::XML::Element.new(name, doc) do |n|
+      match[:a].each do |attrs|
+        attrs.each do |k,v|
+          n[k.to_s] = ((n[k.to_s] || '').split(/\s/) + [v]).join(' ')
+        end
+      end
+      n.add_child(Nokogiri::XML::NodeSet.new(doc, match[:c]))
+    end
+  end
+
 end
