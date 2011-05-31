63f13e46da6613b8395a5ffc3847adc4f7735c72
Jason Garber <jg@jasongarber.com>
2011-05-30 22:50:29 -0600

    Test for successive paragraphs.

--- spec/parser/block_spec.rb
+++ spec/parser/block_spec.rb
@@ -2,14 +2,22 @@ describe RedClothParslet::Parser::Block do
   let(:parser) { described_class.new }
   let(:transform) { RedClothParslet::Transform.new }
   
-  context "undecorated paragraph" do
+  context "undecorated paragraphs" do
     it { should parse("Just plain text.").with(transform).
       as([RedClothParslet::Ast::P.new(["Just plain text."])]) }
   end
 
-  context "explicit paragraph" do
+  context "explicit paragraphs" do
     it { should parse("p. This is a paragraph.").with(transform).
       as([RedClothParslet::Ast::P.new(["This is a paragraph."])]) }
+  end
+  
+  context "successive paragraphs" do
+    it { should parse("One paragraph.\n\nTwo.").with(transform).
+      as([RedClothParslet::Ast::P.new(["One paragraph."]), RedClothParslet::Ast::P.new(["Two."])]) }
+    
+    it { should parse("p. Double.\n\np. Trouble.").with(transform).
+      as([RedClothParslet::Ast::P.new(["Double."]), RedClothParslet::Ast::P.new(["Trouble."])]) }
   end
   
 end
