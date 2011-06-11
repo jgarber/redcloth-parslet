79d6760db5a39794d1a2c00abadb8877156880c1
Jason Garber <jg@jasongarber.com>
2011-06-11 16:10:09 -0600

    Load examples from RedCloth. Oooo buddy, there's a lot to do!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,38 @@ Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.include RedClothParslet::Spec::Matchers
 end
+
+require 'yaml'
+  
+def examples_from_yaml(&block)
+  formatter = description.downcase
+  define_method("format_as_#{formatter}", &block)
+  
+  fixtures.each do |name, doc|
+    if doc[formatter]
+      example("should output #{formatter} for #{name}") do
+        output = method("format_as_#{formatter}").call(doc)
+        output.should == doc[formatter]
+      end
+    else
+      example("should not raise errors when rendering #{formatter} for #{name}") do
+        lambda { method("format_as_#{formatter}").call(doc) }.should_not raise_error
+      end
+    end
+  end
+end
+
+def fixtures
+  return @fixtures if @fixtures
+  @fixtures = {}
+  Dir[File.join(File.dirname(__FILE__), *%w[examples *.yml])].each do |testfile|
+    testgroup = File.basename(testfile, '.yml')
+    num = 0
+    YAML::load_documents(File.open(testfile)) do |doc|
+      name = doc['name'] || num
+      @fixtures["#{testgroup} #{name}"] = doc
+      num += 1
+    end
+  end
+  @fixtures
+end
